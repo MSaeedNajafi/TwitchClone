f400c3019c5523c280846a2162e85c72d815ffbd
Saeed <najafisaeed@gmail.com>
2021-12-19 15:37:18 +0100

    deployed stream chat with no users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
-function App() {
-  return <div className="App">DSAD</div>;
-}
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import "@stream-io/stream-chat-css/dist/css/index.css";
+
+const filters = { type: "gaming" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const client = StreamChat.getInstance("cdrs6vst5auz");
+
+const App = () => {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "dave-matthews",
+            name: "Dave Matthews",
+          },
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.LCquTXyh7B_DPq1itGrRDzB2rWozq0BSOyhE2uIadJY"
+        );
+
+        const channel = await client.channel("gaming", "gamingDemo", {
+          name: "Gaming Demo",
+        });
+        setChannel(channel);
+
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
 export default App;
